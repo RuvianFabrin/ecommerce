fcd5b599e523c9a08e2ddaecff4d39fc98851bc9
Ruvian Fabrin <ruvian.fabrin@hotmail.com>
2019-07-24 20:58:56 -0300

    criando o page admin

---
+++ vendor/hcodebr/php-classes/src/pageAdmin.php
@@ -0,0 +1,7 @@
+<?php
+namespace Hcode;
+class pageAdmin extends \Hcode\Page{
+    public function __construct($opts = array(), $tpl_dir="/views/admin/") {
+        parent::__construct($opts,$tpl_dir);
+        }
+}
